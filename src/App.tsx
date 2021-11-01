4ab74885c6604e5b27dc37b5c13f6824468090d0
OlgaLiubar <olgaliubar@yandex.ru>
2021-11-02 00:15:48 +0300

    feat: delete todo

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,26 @@ import NewTodo from "./components/NewTodo";
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
-  // const todos = [{ id: "t1", text: "Finish the course" }];
-  // const textInputRef = useRef<HTMLInputElement>(null);
-
-  // const todoSubmitHandler = (evt: React.FormEvent) => {
-  //   evt.preventDefault();
-  //   const enteredText = textInputRef.current!.value;
-  //   console.log(enteredText);
-  //   todos.push({id:"t2", text: enteredText});
-  // };
 
   const todoAddHandler = (text: string) => {
-    const todo = {id:Math.random().toString(), text: text};
-    setTodos([todo, ...todos]);
-  }
+    // const todo = {id:Math.random().toString(), text: text};
+    // setTodos([todo, ...todos]);
+    setTodos(prevTodos => [
+      ...prevTodos,
+      {id:Math.random().toString(), text: text}
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter((item) => item.id !== todoId);
+    });
+  };
 
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler}/>
-      <TodoList items={todos} />
+      <TodoList items={todos} onDelete={todoDeleteHandler}/>
     </div>
   );
 }
